712b4e5d71254dff0c9c467add5a83a47946d408
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-06-15 16:52:27 -0700

    set up dashboard and 4 panels

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+
+  render() {
+    return (
+      <section className="loading">Loading</section>
+    );
+  }
+}
+
+export default Loading;
